eeefe551e422906c26bebece14afba1d1c5b921b
Adam <7889445+DMNerd@users.noreply.github.com>
2022-07-17 11:51:34 +0200

    Fixed tome script

--- kubejs/server_scripts/ntp_add_tome.js
+++ kubejs/server_scripts/ntp_add_tome.js
@@ -1,6 +1,7 @@
 // Listen to player login event
 onEvent('player.logged_in', event => {
   if (!event.player.stages.has('starting_items')) {
+    event.player.stages.add('starting_items')
     event.player.give('eccentrictome:tome')
   }
 })
